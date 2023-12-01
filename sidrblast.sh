c320c97c7fc97a4fc1406432fe7a950ceae56892
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2023-12-01 13:31:53 -0500

    Update sidrblast.sh
    NT long format BLAST

--- sidrblast.sh
+++ sidrblast.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos highmem1
 #SBATCH --partition highmem1
 #SBATCH --output=out_blastnt.log
-#SBATCH --mail-user=vegge003@fiu.edu
+#SBATCH --mail-user=your@email.com
 #SBATCH --mail-type=ALL
 
 module load blast-plus-2.7.1-gcc-8.2.0-fm5yf3k
@@ -14,11 +14,10 @@ module load blast-plus-2.7.1-gcc-8.2.0-fm5yf3k
 
 GENOME=/home/data/jfierst/veggers/Oscheius/DF5033/DF5033_hifiAssembly/DF5033ONTpb.asm.bp.p_ctg.fa
 NT=/home/data/jfierst/veggers/nt_db/nt
-NR=
 
 blastx -query ${GENOME} \
-    -db ${NR} \
-    -outfmt 6 \
+    -db ${NT} \
+   # -outfmt 6 \
     -culling_limit 5 \
     -evalue 1e-25 \
     -out SIDRblast.txt
