c667303367c9d43f5e5d19e427ab5581a00b4b4f
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-03-04 13:41:15 -0500

    Add fix blast to sidrblast.sh

--- sidrblast.sh
+++ sidrblast.sh
@@ -22,3 +22,23 @@ blastn -query ${GENOME} \
     -evalue 1e-25 \
     -out SIDRblast.txt
 
+
+################################
+################################
+########FIX_BLAST_OUTPUT########
+################################
+################################
+
+
+awk '/Query=/{print; flag=1; next} flag && /^>/{print; flag=0} flag && /No hits found/{print; flag=0}' out_blastnt.log > test1.txt
+awk 'NR%2==1{col1=$0} NR%2==0{print col1, $0}' test1.txt > test2.txt
+awk '{print $2, $5, $6}' test2.txt > test3.txt
+sed -i 's/hits found/No hits found/g' test3.txt
+sed 's/ /\t/' test3.txt > blastIDs.txt
+rm test*
+sort -k1 blastIDs.txt > blastIDs.txt.temp
+mv blastIDs.txt.temp blastIDs.txt
+cp blastIDs.txt ./stats/.
+echo -e "contig\tOrigin" > header.txt
+cat blastIDs.txt >> header.txt
+mv header.txt blastIDs.txt
